246de120b0e27c9681cfc128560973683ba57589
sreesaidevelopment <ssaaii4499@gmail.com>
2019-11-06 23:28:53 +0530

    Added status code

--- src/test/java/com/avito/RunTest.java
+++ src/test/java/com/avito/RunTest.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.testng.SkipException;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -14,24 +15,30 @@ import io.appium.java_client.android.AndroidDriver;
 
 public class RunTest extends PreConditions{
 
+	public static int csvLine = 0;
+	public static String mobile, password;
 
 	@Test(dataProvider = "getCsvData")
 	public void addingAd(String rooms, String houseType, String floor, String noOfFloors, String ownership,
 			String totalArea, String kitchenArea, String livingArea, String addressLocation,
-			String description, String price, String mobile, String password, String proxyIp, String imagePath, String name) throws IOException, InterruptedException {
+			String description, String price, String proxyIp, String imagePath, String name, String status) throws IOException, InterruptedException {
 		new adbExecutor().exec(udid, proxyIp);
 		logger = extent.startTest("Adding ad in Avito", rooms+","+houseType+","+floor+","+noOfFloors+","+ownership+","+totalArea+","+kitchenArea
-				+","+livingArea+","+addressLocation+","+description+","+price+","+mobile+","+password+","+proxyIp+","+imagePath);
-		launchApp();
-		String[] loginData = createAnAccount(name);
-		mobile = loginData[0];
-		password = loginData[1];
-		openCreateAdForm();
-		enterAdDetails(rooms, houseType, floor, noOfFloors, ownership, totalArea, kitchenArea, livingArea, addressLocation,
-				description, price);
-		attachImageFromGDriver(imagePath);
-		submitCreateAdForm(mobile, password, addressLocation);
-		closeApp();
+				+","+livingArea+","+addressLocation+","+description+","+price+","+proxyIp+","+imagePath+","+status);
+		if (status.equalsIgnoreCase("1")) {
+			launchApp();
+			String[] loginData = createAnAccount(name);
+			mobile = loginData[0];
+			password = loginData[1];
+			openCreateAdForm();
+			enterAdDetails(rooms, houseType, floor, noOfFloors, ownership, totalArea, kitchenArea, livingArea, addressLocation,
+					description, price);
+			attachImageFromGDriver(imagePath);
+			submitCreateAdForm(mobile, password, addressLocation);
+			closeApp();
+		}else {
+			throw new SkipException("Skipped this test case because status for this row is "+status);
+		}
 	}
 	
 	
